e10e548d1c5ab13509f144b5653d288a3d4501c8
Berglopes1204 <lindemberglopes@yahoo.com.br>
2025-11-22 10:38:10 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,34 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+    char Estado;
+    char Código;
+    char cidade;
+    int população;
+    float Área;
+    int PIB;
+    int turisticos;
   // Área para entrada de dados
+    printf("Digite o Estado");
+    scanf('%c', &Estado);
+
+    printf("Digite o Código da cidade");
+    scanf('%c', &Código);
+
+    printf("Digite a Cidade");
+    scanf('%c', &cidade);
+
+    printf("Digite a quantidade da População");
+    scanf('%d', &população);
+
+    printf("Digite a Área em km");
+    scanf('%f', &Área);
+
+    printf("Digite o PIB");
+    scanf('%d', &PIB);
+
+    printf("Digite a quantidade de pontos turísticos");
+    scanf('%d', &turistico);
 
   // Área para exibição dos dados da cidade
 
